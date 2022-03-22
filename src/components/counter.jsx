92956db37df3129181117249d20b4ca00a581f30
Tanmay Pilla <tpilla@uwaterloo.ca>
2022-03-22 00:13:45 -0400

    added first bootstrap

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,13 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/400",
   };
   render() {
     return (
       <div>
         <h1> ShopBasket</h1>
-        <img src={this.state.imageUrl} alt="" />
-        <span> {this.formatCount()}</span>
+
+        <span className="badge badge-primary"> {this.formatCount()}</span>
         <button> Increment</button>
       </div>
     );
